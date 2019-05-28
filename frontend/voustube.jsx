d20609960c638299c6c57353a841e9500679f7cb
Alexandro <alexandro.strauss@gmail.com>
2019-05-28 12:07:38 -0400

    finish initializing skeleton

---
+++ frontend/voustube.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReactDOM.render(<h1>Welcome To V O U S T U B E</h1>, root);
+});
