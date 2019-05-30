b59e1ed10ede816377a703423c0b3fef1081f7a4
Alexandro <alexandro.strauss@gmail.com>
2019-05-30 14:05:34 -0400

    fully functioning errors

--- frontend/components/users/signup_form.jsx
+++ frontend/components/users/signup_form.jsx
@@ -11,9 +11,11 @@ class SignupForm extends React.Component {
             username: "",
             email: "",
             password: "",
-            showPwd: false
+            showPwd: false,
+            submitted: false,
         };
-        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.isEmpty = this.isEmpty.bind(this);
     }
 
     update(field) {
@@ -25,31 +27,93 @@ class SignupForm extends React.Component {
     handleSubmit(e) {
         e.preventDefault();
         const user = Object.assign({}, this.state);
-        this.props.processForm(user);
+        this.props.processForm(user).then(this.setState({submitted: true}));
         <Redirect to={`/users/${user.id}`} />
+    }
+
+    isEmpty(string) {
+        if (string === "" && this.state.submitted) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    passwordShort() {
+        if (this.state.password.length < 8 && this.state.submitted){
+            return true
+        } else {
+            return false
+        }
+    }
+
+    passwordError() {
+        if (this.state.password.length < 8 && this.state.submitted) {
+            if (this.state.password === "") {
+                return (
+                    <div className="error">
+                        <img src="/assets/caution_symbol.png" height="20" width="20" />
+                        <p>Enter a password</p>
+                    </div>
+                )
+            } else {
+                return (
+                    <div className="error">
+                        <img src="/assets/caution_symbol.png" height="20" width="20" />
+                        <p>Use 8 characters or more for your password</p>
+                    </div>
+                )
+            }
+        } else {
+        return (
+            <p>Use 8 or more characters with a mix of letters, numbers & symbols</p>
+        )
+        }
     }
 
     render() {
         return (
             <div className="toplevel-signup">
-            <div className = "signup-form">
+            <div className="signup-form">
                 
-                <p className="welcoming">Sign Up</p>
+                <p className="welcoming">Create your VousTube Account</p>
                 <p className="continue">to continue to VousTube</p>
                 <form onSubmit={this.handleSubmit} >
-                    <div className="username">
-                        <label>Your name</label>
+                        <div className={this.isEmpty(this.state.username) ? "floating-label-error" : "floating-label"}>
+                        {/* <label>Your name</label> */}
                         <input type="text" placeholder="Your name" value={this.state.username} onChange={this.update('username')} />
+                            
+                            {this.isEmpty(this.state.username) ?
+                                <div className="error">
+                                    <img src="/assets/caution_symbol.png" height="20" width="20" />
+                                    <p>Enter a username</p>
+                                </div>
+                                :
+                                <> </>
+                            }
                     </div>
 
-                    <div className="email">
-                        <label>Your email address</label>
+                        <div className={this.isEmpty(this.state.email) ? "floating-label-error" : "floating-label"}>                        
+                        {/* <label>Your email address</label> */}
                             <input type="email" placeholder="Your email address" value={this.state.email} onChange={this.update('email')} />
+                            {this.isEmpty(this.state.email) ?
+                                <div className="error">
+                                    <img src="/assets/caution_symbol.png" height="20" width="20" />
+                                    <p>Choose an email address</p>
+                                </div>
+                                :
+                                <> </>
+                            }
                     </div>
 
+                        <div className={this.passwordShort() ? "floating-label-error" : "floating-label"}>                        
+                            {/* <label>Password</label> */}
+                            <input type={this.state.showPwd ? "text" : "password"} placeholder="Password" value={this.state.password} onChange={this.update('password')} />
 
-                    <label>Password<input type={this.state.showPwd ? "text" : "password"} placeholder="Password" value={this.state.password} onChange={this.update('password')} /></label>
-                    <p>Use 8 or more characters with a mix of letters, numbers & symbols</p>
+                            {this.passwordError()} 
+                    </div>
+                    
+                    
                     <div className="bottom-links">
                         <Link to="/login">Sign in instead</Link>
                         <input type="submit" className="submit" value="Next" onClick={this.handleSubmit} />
@@ -64,3 +128,18 @@ class SignupForm extends React.Component {
 }
 
 export default SignupForm
+
+
+{/* () ?
+                                this.isEmpty(this.state.password) ?
+                                    <div className="error">
+                                        <img src="/assets/caution_symbol.png" height="20" width="20" />
+                                        <p>Enter a password</p>
+                                    </div>
+                                :
+                                    <div className="error">
+                                        <img src="/assets/caution_symbol.png" height="20" width="20" />
+                                        <p>Use 8 characters or more for your password</p>
+                                    </div>                                    
+                             :   <p>Use 8 or more characters with a mix of letters, numbers & symbols</p>
+                            } */}
