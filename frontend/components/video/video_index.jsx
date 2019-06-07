3ae2223745a4c133b24a741bb365219c68dabb5f
Alexandro <alexandro.strauss@gmail.com>
2019-06-07 11:09:28 -0400

    bootstrapped images for heroku purposes

--- frontend/components/video/video_index.jsx
+++ frontend/components/video/video_index.jsx
@@ -43,7 +43,7 @@ class VideoIndex extends React.Component {
             return(
             <li className="video">
                 <a className="video-tile" href={`/#/videos/${video.id}`}>
-                    <img src={video.imageUrl ? video.imageUrl : asset_path("e.png")} />
+                        <img src={video.imageUrl ? video.imageUrl : window.defaultImg} />
                     <div className="vid-info">
                         <div className="duration">{video.duration}</div>
                         <div className="title">{video.title}</div>     
